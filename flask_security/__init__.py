08e1bfb3f0296a687311e282dbe40ee5efba784d
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-01-19 15:38:19 +0100

    release: v3.1.4

--- flask_security/__init__.py
+++ flask_security/__init__.py
@@ -36,7 +36,7 @@ from .signals import confirm_instructions_sent, password_reset, \
     reset_password_instructions_sent, user_confirmed, user_registered
 from .utils import login_user, logout_user, url_for_security
 
-__version__ = '3.1.3'
+__version__ = '3.1.4'
 __all__ = (
     'AnonymousUser',
     'auth_required',
